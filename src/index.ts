913b38c4c7b1527189b0a24abd7c9e29bb5db8f8
Ming Ki Chong <unexpected.exception@gmail.com>
2020-12-10 00:16:20 -0800

    add object serialisation with sorted keys

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import * as crypto from 'crypto';
 import * as fs from 'fs';
-import sortObject from 'sort-object-keys';
+import { isArray, isPlainObject as _isPlainObject } from 'lodash';
 
 import { logger } from './logger';
 
@@ -12,16 +12,34 @@ const pubKey = pubPem.toString('ascii');
 
 const data = {
   a: true,
-  e: [{ v: 'w', z: 'y', f: 'g' }, { h: ['j', 'i'] }],
-  b: { c: 'd' },
-  x: 'x',
+  e: [{ v: 1, z: 1, f: { t:1, p:1, s:1 } }, { h: ['o', 'j', 'i'] }],
+  b: { c: 1 },
+  x: 1,
 };
 
-const dataString = JSON.stringify(sortObject(data));
+const dataString = JSON.stringify(sortObjectKeysRecursively(data));
 logger.info(dataString);
 
 const signature = crypto.createSign('sha256').update(dataString).sign(secret, 'base64');
 logger.info(signature);
 
-const result = crypto.createVerify('sha256').update(JSON.stringify(sortObject(data))).verify(pubKey, signature, 'base64');
+const result = crypto.createVerify('sha256').update(JSON.stringify(sortObjectKeysRecursively(data))).verify(pubKey, signature, 'base64');
 logger.info(result);
+
+function sortObjectKeysRecursively(obj: unknown, sortFn?: (a:string, b:string) => number): unknown {
+  if (isPlainObject(obj)) {
+    const newObject: Record<string, unknown> = {};
+    for(const key of Object.keys(obj).sort()){
+      newObject[key] = sortObjectKeysRecursively(obj[key], sortFn);
+    }
+    return newObject;
+  } else if(isArray(obj)){
+    return obj.map((item) => sortObjectKeysRecursively(item), sortFn);
+  } else {
+    return obj;
+  }
+}
+
+function isPlainObject(maybeObj: any): maybeObj is Record<string, any> {
+  return _isPlainObject(maybeObj);
+}
